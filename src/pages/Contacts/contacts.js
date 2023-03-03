b103f4adb6539a2a515c96a3e9c905f41c97b627
Aidar06 <kabylbekovajdar94@gmail.com>
2023-03-03 21:22:37 +0600

    someFex

--- src/pages/Contacts/contacts.js
+++ src/pages/Contacts/contacts.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import img from "../../assets/img/Rectangle 7.png"
-import room from "../../assets/img/room.png"
+import {IoCallOutline} from "react-icons/io5";
+import {HiOutlineMail} from "react-icons/hi";
+import {GrInstagram} from "react-icons/gr";
+import {SiGooglemaps} from "react-icons/si";
 const Contacts = () => {
     return (
-        <div>
-            <div id="about">
-                <img className="img" src={img} alt=""/>
-                <div className="about">
-                    <div className="about--two">
-                        <h1 className="about--two__tittle">About us</h1>
-                        <p className="about--two__paragraph"> Reading is not a pure “receptive” process. To read a book is not <br/>
-                            just to remember the plot. Reading is also a productive, <br/>
-                            productive process, since during reading we pronounce the <br/>
-                            words we read and comprehend what we read. The frontal lobes <br/>
-                            of the left hemisphere of the brain are responsible for <br/>
-                            comprehension. People with frontal lobe injuries cannot, <br/>
-                            for example, create semantic guesses, think through what they <br/>
-                            have read and build a reading strategy. together, this leads <br/>
-                            to a misunderstanding of what is being read.</p>
+        <section id='contact'>
+            <div className='container'>
+                <div className='contact'>
+                    <div className='contact--block'>
+                        <IoCallOutline className='contact--block__icon1'/>
+                        <h4>Phone number:</h4>
+                        <p>(+996)557-30-02-30</p>
+                        <p>(+996)220-42-42-08</p>
                     </div>
-                    <img className="about--img2" src={room} alt=""/>
+                    <div className='contact--block'>
+                        <HiOutlineMail className='contact--block__icon1'/>
+                        <h4>Email address:</h4>
+                        <a href="https://instagram.com/motion_web?igshid=YmMyMTA2M2Y=">motion@mail.com</a>
+                    </div>
+                    <div className='contact--block'>
+                        <GrInstagram className='contact--block__icon1'/>
+                        <h4>Instagram:</h4>
+                        <a href="">@bookshop</a>
+                    </div>
+                    <div className='contact--block'>
+                        <SiGooglemaps className='contact--block__icon1'/>
+                        <h4>Address:</h4>
+                        <p>Bishkek, Kievskaya st., 152/5</p>
+                    </div>
+                </div>
+                <div className='map'>
+                    <iframe className='map--block'
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2924.2525145016607!2d74.5819435151655!3d42.867514110896266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389ec825790b32f5%3A0x7545c2a8e37ec365!2zMTA5LzMg0YPQuy4g0KLRg9GA0YPRgdCx0LXQutC-0LLQsCwg0JHQuNGI0LrQtdC6IDcyMDAwMQ!5e0!3m2!1sru!2skg!4v1677674527804!5m2!1sru!2skg"
+                            height="450" allowFullScreen="" loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
