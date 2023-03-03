03bba6d9da5262233a5097962c19364b2547c4a1
Aidar06 <kabylbekovajdar94@gmail.com>
2023-03-03 21:11:43 +0600

    inchang

--- src/components/Home/HomeBooks/homeBooks.js
+++ src/components/Home/HomeBooks/homeBooks.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const HomeBooks = () => {
+
+    const [books,serBooks] = useState({})
+
+    const gitBooks= async()=> {
+        try {
+            const res = await axios('https://motionbookshop3.herokuapp.com/product/books/')
+            const {data} = await res
+            await serBooks(data)
+        }catch (e){
+            console.log(e,'error')
+        }
+    }
+
+    useEffect(()=> {
+        gitBooks()
+    }, [])
+
+    console.log(books)
+
     return (
         <div>
 
