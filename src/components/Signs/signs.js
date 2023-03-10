b091ad79def9dc2349326daa915ffe484bc0a4ce
Aidar06 <kabylbekovajdar94@gmail.com>
2023-03-10 22:34:54 +0600

    signs

--- src/components/Signs/signs.js
+++ src/components/Signs/signs.js
@@ -22,6 +22,90 @@ const Signs = ({signUp,setSignUp,setSignIn,signIn}) => {
 
     const [eyes,setEyes] = useState(true)
 
+    const [name,setName] = useState('')
+    const [gmail,setGmail] = useState('')
+    const [password,setPassword] = useState('')
+    function creatAccount(){
+        let acc = JSON.parse(localStorage.getItem('accounts')) || []
+
+        let newAcc = {
+            name: name,
+            gmail: gmail,
+            id: acc.length + 1,
+            password: password,
+            inAcc: true
+        }
+
+        let setAcc = localStorage.setItem('accounts',JSON.stringify([...acc,newAcc]))
+    }
+
+    const [incorrectPassword,setIncorrectPassword] = useState(false)
+    const [notAccount,setNotAccount] = useState(false)
+
+    const [inGmail,setInGmail] = useState('')
+    const [inPassword,setInPassword] = useState('')
+    function inAccount(){
+        let acc = JSON.parse(localStorage.getItem('accounts')) || []
+        let inAcc = []
+        acc.map(el => {
+            if (el.gmail === inGmail  && el.password === inPassword){
+                acc.map(el => {
+
+                    if (el.gmail === inGmail && el.password === inPassword){
+                        inAcc.push(
+                            {
+                                name: el.name,
+                                gmail: el.gmail,
+                                id: el.id,
+                                password: el.password,
+                                inAcc: true
+                            }
+                        )
+                    }else {
+                        inAcc.push(
+                            {
+                                name: el.name,
+                                gmail: el.gmail,
+                                id: el.id,
+                                password: el.password,
+                                inAcc: el.inAcc
+                            }
+                        )
+                    }
+                    inLeave()
+                    setSignTime(false)
+                    let setAcc = localStorage.setItem('accounts',JSON.stringify(inAcc))
+                })
+            }else if (el.gmail === inGmail  && el.password !== inPassword){
+                setIncorrectPassword(true)
+            }else if(acc.map(element => {if(element.gmail === inGmail){return true}else{return false}}).includes(true) === false){
+                setNotAccount(true)
+            }
+
+        })
+
+    }
+
+    const [inInput,setInInput] = useState(false)
+
+    function changeInInput(){
+        setInInput(true)
+        setTimeout(()=> {
+            setInInput(false)
+        }, 3000)
+    }
+
+    function signInFunc(){
+        inAccount()
+    }
+
+    function signUpFunc(){
+        creatAccount()
+        upLeave()
+        setSignTime(false)
+    }
+
+
     return (
         <div style={{
             display: signIn || signUp ? '': 'none',
@@ -31,24 +115,26 @@ const Signs = ({signUp,setSignUp,setSignIn,signIn}) => {
                 <div style={{
                     display: signIn? '' : 'none',
                 }} className={`signs--group__block ${signTime? 'gr-ani-come': 'gr-ani-leave'}`}>
+                    <h2 style={{display: notAccount? '' : 'none'}} className='signs--group__block--text'>No such account!</h2>
                     <div onClick={()=> {inLeave();setSignTime(false)}} className="signs--group__block--close">
                         <IoCloseOutline/>
                     </div>
                     <h1>Log in</h1>
                     <div className="signs--group__block--input">
                         <h3>E-mail address</h3>
-                        <input type="email"/>
+                        <input style={{border: inInput && inGmail.length === 0? '1px solid red' : ''}} onChange={(e)=> {setInGmail(e.target.value); setNotAccount(false)}} type="email"/>
                     </div>
                     <div className="signs--group__block--input">
-                        <h3>Password</h3>
-                        <input type={eyes? 'password' : 'text'}/>
+                        <h3>Password <span>{incorrectPassword? 'incorrect password' : ''}</span></h3>
+                        <input style={{border: inInput && inPassword.length === 0 || incorrectPassword? '1px solid red' : ''}} onChange={(e)=> {setInPassword(e.target.value); setIncorrectPassword(false)}} type={eyes? 'password' : 'text'}/>
                         <div onClick={()=> setEyes(!eyes)} className='signs--group__block--input__icon'>
                             {
                                 eyes? <IoEyeSharp/>: <BsFillEyeSlashFill/>
                             }
                         </div>
+                        <h4>forgot password</h4>
                     </div>
-                    <button>Sign in</button>
+                    <button onClick={()=> inGmail.length !== 0 && inPassword.length !== 0 ? signInFunc()  : changeInInput()}>Sign in</button>
                 </div>
                 <div style={{
                     display: signUp? '': 'none'
@@ -59,22 +145,22 @@ const Signs = ({signUp,setSignUp,setSignIn,signIn}) => {
                     <h1>Get started with an <br/>account</h1>
                     <div className="signs--group__block--input">
                         <h3>Name</h3>
-                        <input type="email"/>
+                        <input style={{border: inInput && name.length === 0? '1px solid red' : ''}} onChange={(e)=> setName(e.target.value)} type="email"/>
                     </div>
                     <div className="signs--group__block--input">
                         <h3>E-mail address</h3>
-                        <input type="email"/>
+                        <input style={{border: inInput && gmail.length === 0? '1px solid red' : ''}} onChange={(e)=> setGmail(e.target.value)} type="email"/>
                     </div>
                     <div className="signs--group__block--input">
                         <h3>Password</h3>
-                        <input type={eyes? 'password' : 'text'}/>
-                        <div onClick={()=> setEyes(!eyes)} className='signs--group__block--input__icon'>
+                        <input style={{border: inInput && password.length === 0? '1px solid red' : ''}} onChange={(e)=> setPassword(e.target.value)} type={eyes? 'password' : 'text'}/>
+                        <div onClick={()=> setEyes(!eyes)} className='signs--group__block--input__icon1'>
                             {
                                 eyes? <IoEyeSharp/>: <BsFillEyeSlashFill/>
                             }
                         </div>
                     </div>
-                    <button>Sign up</button>
+                    <button onClick={()=> gmail.length !== 0 && password.length !== 0 && name.length !== 0 ? signUpFunc()  : changeInInput()}>Sign up</button>
                 </div>
             </div>
         </div>
